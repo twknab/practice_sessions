b8e56b2e4a6f5e2296c57c7aa55e943f38b4a949
twknab <20636750+twknab@users.noreply.github.com>
2017-12-13 15:22:29 -0800

    Updated algorithm and removed duplicate <script> tag

--- session_1/main.js
+++ session_1/main.js
@@ -1,8 +1,8 @@
 // Assemble Our New Table
 /*
   1. Iterate through each row.
-  2. Iterate through each cell, and pushing data to a row array.
-  3. Push each row array into our new table.
+  2. Iterate through each cell, and push row data into an array.
+  3. Push each row's array into our new table array.
   (This means we have an array, containing each (many) row(s) as an array.)
 */
 var table = document.getElementById("myTable");
@@ -10,49 +10,94 @@ var newTable = [];
 
 for (let i = 1, row; row = table.rows[i]; i++) {
   let rowData = [];
-   //iterate through rows
-   //rows would be accessed using the "row" variable assigned in the for loop
+   // Iterate through rows:
    for (let j = 0, col; col = row.cells[j]; j++) {
-     // console.log(row.cells[j].innerHTML);
      rowData.push(row.cells[j].innerHTML);
+     // `innerHTML` contains cell data as a string.
    }
 
    newTable.push(rowData);
 };
 
-// Find Table Data by Country:
-function getInfo(country){
-  var foundResults = [];
+/*
+  Right now we have assembled one big array, which contains many sub-arrays, each containing row data.
 
-  for (let i = 0; i < newTable.length; i++) { // goes through Row - Company/Contact etc.
-    for (let j = 0; j < newTable[i].length; j++){ // goes through data within Company/Contact etc.
-      if (newTable[i][j] == country) {
-        // Do some stuff once we find the country
-        // Do we want to do our switch case here? Or somewhere else:
+  So we've solved the problem of how to extract data from a table. But how do we now search through that data and aggregate it?
+*/
 
-        // See it below:
-        /*
+function findCompaniesByCountry(country) {
+  /*
+  Looks through our New Table array looking for any sub-array which contains our chosen country.
 
-        switch (country) { // depending on which COUNTRY is selected, Germany.. etc. etc. it spits out information??
+  Parameters:
+  - `country`: The country wishing to search for, as a string.
+  */
 
-          case foundCountry:
-            console.log(`Country found. ${foundContact} works for ${foundCompany} in ${foundCountry}`);
-            // let company = newTable[i][j-2];
-            // let contact = newTable[i][j-1];
-            // let country = newTable[i][j];
-            // console.log(`Country Searched: ${country}. Company ${company} and contact is ${contact}`);
-            break;
-          default: // if none of the cases are Countries. say "sorry you fucked up"
-            // console.log("Not found.");
-            break;
-        }
+  // Holds any found results:
+  var results = [];
 
-        */
-      }
-      // Stuff after the switch statement if there was any
-    };
+  // Loop through new table looking to see if the 2nd index place matches our country. Note: The 2nd index place, because of our table, is always a country name. If the table format was to change, this algorithm would have to be updated. (or we could try an alternative approach similar to our first attempt below):
+  for (var i = 0; i < newTable.length; i++) {
+    switch (country) {
+      case newTable[i][2]: // Matches for 2nd index place
+        console.log("Country found!");
+        // Push an object containg our found listing to our results array:
+        results.push({
+          company: newTable[i][0],
+          contact: newTable[i][1],
+          country: newTable[i][2],
+        })
+        break;
+      default:
+        break;
+    }
   }
+
+  console.log(results);
+  return results;
 };
+
+// console.log(findCompaniesByCountry('Germany'));
+findCompaniesByCountry('Germany');
+
+
+//-----------------------------//
+//------- ATTEMPT ONE ---------//
+//-----------------------------//
+
+// Find Table Data by Country:
+// function getInfo(country){
+//   var foundResults = [];
+//
+//   for (let i = 0; i < newTable.length; i++) { // goes through Row - Company/Contact etc.
+//     for (let j = 0; j < newTable[i].length; j++){ // goes through data within Company/Contact etc.
+//       if (newTable[i][j] == country) {
+//         // Do some stuff once we find the country
+//         // Do we want to do our switch case here? Or somewhere else:
+//
+//         // See it below:
+//         /*
+//
+//         switch (country) { // depending on which COUNTRY is selected, Germany.. etc. etc. it spits out information??
+//
+//           case foundCountry:
+//             console.log(`Country found. ${foundContact} works for ${foundCompany} in ${foundCountry}`);
+//             // let company = newTable[i][j-2];
+//             // let contact = newTable[i][j-1];
+//             // let country = newTable[i][j];
+//             // console.log(`Country Searched: ${country}. Company ${company} and contact is ${contact}`);
+//             break;
+//           default: // if none of the cases are Countries. say "sorry you fucked up"
+//             // console.log("Not found.");
+//             break;
+//         }
+//
+//         */
+//       }
+//       // Stuff after the switch statement if there was any
+//     };
+//   }
+// };
 
 // // var area = {
 //   Company: "Alfreds Futterkiste"
@@ -60,4 +105,4 @@ function getInfo(country){
 //   Country: "Germany"
 // }
 
-getInfo("Germany");
+// getInfo("Germany");
